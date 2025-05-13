d04cc5e77305d0f78f7bbf46357552b2ce11842b
FarazBisati <fbisati@gmail.com>
2025-05-13 20:42:03 +0530

    serial yml file changes

--- cypress.config.js
+++ cypress.config.js
@@ -17,12 +17,19 @@ module.exports = defineConfig({
       const { defineConfig } = require("cypress");
 
       module.exports = defineConfig({
+        reporter: "cypress-mochawesome-reporter",
+        reporterOptions: {
+          reportDir: "cypress/reports/html",
+          overwrite: false,
+          html: true,
+          json: true,
+        },
         pageLoadTimeout: 100000,
         retries: {
           runMode: 2,
           openMode: 2,
         },
-        reporter: "cypress-mochawesome-reporter",
+
         e2e: {
           baseUrl: "https://automationteststore.com/",
           video: true,
