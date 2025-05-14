32907e15e4aee5a5d93b91ddd6f73994a53d00e5
FarazBisati <fbisati@gmail.com>
2025-05-14 17:53:19 +0530

    new yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,46 +1,30 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "8gr6z5",
   pageLoadTimeout: 100000,
   retries: {
     runMode: 2,
     openMode: 2,
   },
   reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports/html",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     baseUrl: "https://automationteststore.com/",
     video: true,
     videoCompression: 32,
     videosFolder: "cypress/reports/videos",
     screenshotOnRunFailure: true,
+
     setupNodeEvents(on, config) {
-      const { defineConfig } = require("cypress");
+      require("cypress-mochawesome-reporter/plugin")(on);
 
-      module.exports = defineConfig({
-        reporter: "cypress-mochawesome-reporter",
-        reporterOptions: {
-          reportDir: "cypress/reports/html",
-          overwrite: false,
-          html: true,
-          json: true,
-        },
-        pageLoadTimeout: 100000,
-        retries: {
-          runMode: 2,
-          openMode: 2,
-        },
-
-        e2e: {
-          baseUrl: "https://automationteststore.com/",
-          video: true,
-          videoCompression: 32,
-          videosFolder: "cypress/reports/videos",
-          screenshotOnRunFailure: true,
-          setupNodeEvents(on, config) {
-            require("cypress-mochawesome-reporter/plugin")(on);
-          },
-        },
-      });
+      return config;
     },
   },
 });
